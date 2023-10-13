bd9dea4943d7202b7b50367c325c54d4029a4367
Edocico <edoardo.cico@gmail.com>
2023-10-13 15:28:32 +0200

    fix style class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log('ok')
+
+// Recupero elementi dal DOM
+
+const gridDOMElement = document.querySelector('.grid')
+console.log(gridDOMElement)
+const selectDOMElement = document.getElementById('difficulty-select')
+console.log(selectDOMElement)
+console.log(selectDOMElement.value)
+const playBtnDOMElement = document.querySelector('.play-btn')
+console.log(playBtnDOMElement)
