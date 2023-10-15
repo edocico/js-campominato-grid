8c21fc5faa3a632696db84502217e9cff30d3f4d
Edocico <edoardo.cico@gmail.com>
2023-10-15 13:05:55 +0200

    removed console log

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-console.log('ok')
+//console.log('ok')
 
 // Recupero elementi dal DOM
 
 const gridDOMElement = document.querySelector('.grid')
-console.log(gridDOMElement)
+
 const selectDOMElement = document.getElementById('difficulty-select')
-console.log(selectDOMElement)
-console.log(selectDOMElement.value)
+
 const playBtnDOMElement = document.querySelector('.play-btn')
-console.log(playBtnDOMElement)
+
 
 
 playBtnDOMElement.addEventListener('click', function() {
